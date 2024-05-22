3eb9a3796a1389947781efcee4986457ea7ee1c3
Jyotsna gavtade <107462951+Jyo20@users.noreply.github.com>
2024-05-22 11:41:40 +0000

    Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
 const Home = () => {
   return <>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,13 +8,15 @@ const NavBar = ({ isLogged }) => {
     return (
       <>
         <div className="login-div">
-          <NavLink to={"/user"}className={"NoLink"}><FaUser />Hello User</NavLink>
+          <NavLink to={"/user"} className={"NoLink"}>
+            <FaUser />
+            <span>Hello, User</span>
+          </NavLink>
           <NavLink to={"/logout"}>Logout</NavLink>
         </div>
-
       </>
-    )
-  }
+    );
+  };
   let Guest = () => {
     return (
       <>
@@ -22,25 +24,28 @@ const NavBar = ({ isLogged }) => {
           <NavLink to={"/sign"}>Signup</NavLink>
           <NavLink to={"/login"}>Login</NavLink>
         </div>
-
       </>
-    )
-  }
-
+    );
+  };
 
   return (
     <div className="container">
-      <div className="nav-container">
-        <NavLink to={"/"} className={"NoLink"}>ESHOP</NavLink>
+      <div className="left-nav">
+        <NavLink to={"/"} className={"NoLink"}>
+          ESHOP
+        </NavLink>
       </div>
-      <div className="navBar">
+      <div className="middle-navBar">
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/Products"}>Products</NavLink>
         <NavLink to={"/About"}> About Us</NavLink>
         <NavLink to={"/Contact"}> Contact US</NavLink>
+      </div>
+      <div className="right-nav">
         {isLogged === true ? <User /> : <Guest />}
-        <NavLink to={"/Cart"} className={"NoLink"}><FaShoppingCart /></NavLink>
-
+        <NavLink to={"/Cart"} className={"NoLink"}>
+          <FaShoppingCart />
+        </NavLink>
       </div>
     </div>
   );
