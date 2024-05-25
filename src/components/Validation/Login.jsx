c82dae54d5c4b5e751a6100442e3e541b9308d83
Jyotsna gavtade <107462951+Jyo20@users.noreply.github.com>
2024-05-25 16:37:40 +0000

    Added Login page

--- src/components/Validation/Login.jsx
+++ src/components/Validation/Login.jsx
@@ -1,85 +1,40 @@
 import React from "react";
 import "./validation.css";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import loginImage from "../../Images/login.jpg";
 const Login = () => {
-  let { visible, setVisible } = useState(false);
   return (
     <>
-      <div className="main-container">
-        <button>Login</button>
-        <button onClick={()=>{
-            return setVisible(!visible)
-        }} >Signup</button>
-        <div className="login-container">
-          <form>
-            <h2>Login</h2>
-            <div>
-              {" "}
-              <label htmlFor="">Username</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Enter Username"
-              />
-            </div>
-            <div>
-              <label htmlFor="">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Enter Password"
-              />
-            </div>
-            <button type="submit">Login</button>
-          </form>
-        </div>
-        {/****************signup***************************/}
-
-        <div className="signup-container">
-          <form>
-            <div>
-              {" "}
-              <label htmlFor="">Username</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Enter Username"
-              />
-            </div>
-            <div>
-              <label htmlFor="">Email Id</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Enter Email"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Enter Password"
-              />
-            </div>
-            <div>
-              <label htmlFor="">Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                placeholder="Re-enter password"
-              />
-            </div>
-            <button type="submit">Register</button>
-          </form>
-        </div>
+      <div className="login-container">
+        <img src={loginImage} alt="" />
+        <form>
+          <div className="logo">
+            <span>Welcome to</span>
+            <NavLink to={"/"} className={"NoLink"}>
+              ESHOP
+            </NavLink>
+          </div>
+          <div>
+            {" "}
+            <label htmlFor="">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter Username"
+            />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
     </>
   );
--- src/components/Validation/Signup.jsx
+++ src/components/Validation/Signup.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
-
+import "./validation.css";
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <>
+    
+    <div className="signup-container">
+          <h2>SignUp Form</h2>
+          <form>
+            <div>
+              {" "}
+              <label htmlFor="">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter Username"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Email Id</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter Email"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter Password"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Re-enter password"
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+        ]</>
   )
 }
