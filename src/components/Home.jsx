8e41abb5a5563ecd3510393e1ee56db1bf65799a
Jyotsna gavtade <107462951+Jyo20@users.noreply.github.com>
2024-05-27 16:16:28 +0000

    Update Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,18 +6,28 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import image1 from "../Images/image1.jpg";
 import image2 from "../Images/image2.jpg";
 import image3 from "../Images/image3.jpg";
+import image4 from "../Images/image4.jpg"
 
 const Home = () => {
   return (
-    <Carousel infiniteLoop>
-      <div>
+    /**infiniteLoop autoPlay interval={2000} */
+    <Carousel infiniteLoop autoPlay interval={2000}>
+      <div className="text">
         <img src={image1} alt="" />
+        <div>
+          <h1>Let's <br /> Explore <br />
+          <span>unique</span> <br />clothes </h1>
+          <button>Shop Now</button>
+        </div>
       </div>
       <div>
         <img src={image2} alt="" />
       </div>
       <div>
         <img src={image3} alt="" />
+      </div>
+      <div>
+        <img src={image4} alt="" />
       </div>
     </Carousel>
   );
